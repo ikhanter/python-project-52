49c950e4001e90c4038a1e19c38e7ffb676a75c8
Yulian <khantermail@gmail.com>
2023-10-03 16:08:49 +0300

    try new allowed host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['webserver', '127.0.0.1', 'pageanalyzer-production.up.railway.app']
+ALLOWED_HOSTS = ['pageanalyzer-production.up.railway.app', 'webserver', '127.0.0.1']
 
 
 # Application definition
