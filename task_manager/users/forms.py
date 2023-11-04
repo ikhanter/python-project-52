c6e16a3568a3d6399ddf5becfb79b1c7da570c2e
ikhanter <khantermail@mail.ru>
2023-11-04 22:13:34 +0300

    fix

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -5,9 +5,19 @@ from django.utils.translation import gettext_lazy
 
 
 class UsersCreateForm(forms.ModelForm):
-    confirm_password = forms.CharField(
+    password1 = forms.CharField(
+        label=gettext_lazy('Password'),
         widget=forms.PasswordInput(attrs={
-            'placeholder': gettext_lazy('Confirm password')
+            'placeholder': gettext_lazy('Password')
+            },
+        ),
+        validators=[MinLengthValidator(3)],
+        help_text=gettext_lazy('Your password must contain at least 3 symbols.')
+    )
+    password2 = forms.CharField(
+        label=gettext_lazy('Confirm password'),
+        widget=forms.PasswordInput(attrs={
+            'placeholder': gettext_lazy('Confirm password'),
             }
         ),
         validators=[MinLengthValidator(3)],
@@ -15,12 +25,11 @@ class UsersCreateForm(forms.ModelForm):
 
     class Meta:
         model = CustomUser
-        fields = ['first_name', 'last_name', 'username', 'password']
+        fields = ['first_name', 'last_name', 'username']
         labels = {
             'first_name': gettext_lazy('First name'),
             'last_name': gettext_lazy('Last name'),
             'username': gettext_lazy('Username'),
-            'password': gettext_lazy('Password'),
         }
         widgets = {
             'first_name': forms.TextInput(
@@ -41,27 +50,21 @@ class UsersCreateForm(forms.ModelForm):
                     'class': 'mb-3'
                 }
             ),
-            'password': forms.PasswordInput(
-                attrs={
-                    'placeholder': gettext_lazy('Password')
-                },
-            ),
         }
         help_texts = {
             'username': gettext_lazy('Required field. Max length is 150 symbols. Letters, digits and  @/./+/-/_ symbols are allowed.'),  # noqa: E501
-            'password': gettext_lazy('Your password must contain at least 3 symbols.'),  # noqa: E501
         }
 
     def clean_password(self):
-        password1 = self.data['confirm_password']
-        password2 = self.data['password']
-        if password1 and password2 and password1 != password2:
+        password1 = self.data['password1']
+        password2 = self.data['password2']
+        if password1 and password1 != password2:
             raise forms.ValidationError('Passwords do not match.')
         return password2
 
     def save(self, commit=True):
         user = super().save(commit=False)
-        user.set_password(self.cleaned_data['password'])
+        user.set_password(self.cleaned_data['password1'])
         if commit:
             user.save()
         return user
